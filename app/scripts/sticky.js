0b3de1097ad30ebdb1dc87b9ca46856ab53dc76b
mrhanti <mr.el.hanti@gmail.com>
2013-11-11 19:58:42 +0100

    add sticky module file

---
+++ app/scripts/sticky.js
@@ -0,0 +1,80 @@
+(function($, exports) {
+
+    function Sticky(element, options) {
+        this.options  = $.extend({}, Sticky.DEFAULTS, options);
+        this.$window  = $(window);
+
+        this.$element = $(element);
+
+        this.pinned = false;
+
+        this.$window.on('scroll', $.proxy(this.check, this));
+
+
+        // To delete later on
+        $('.show-more').click(function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            $('.text').css({'height': '800px', 'overflow': ''});
+            return;
+        });
+
+        $('.show-less').click(function(e) {
+            e.preventDefault();
+            e.stopPropagation();
+
+            $('.text').css({'height': '400px', 'overflow': 'hidden'});
+            return;
+        });
+
+        // this.check();
+    }
+
+    Sticky.DEFAULTS = {
+        bottomClassName: 'bottom-fixed',
+        topClassName:    'top-fixed'
+    };
+
+    Sticky.prototype.check = function() {
+        var
+        options      = this.options,
+        offset       = this.$element.offset(),
+
+        scrollHeight = offset.top + this.$element.height(),
+        scrollTop    = this.$window.scrollTop(),
+        screenHeight = this.$window.height();
+
+        console.log(this.$element.height());
+        if(scrollHeight <= screenHeight && !this.$element.hasClass(options.topClassName)) {
+            this.$element.addClass(options.topClassName);
+            return;
+        }
+
+        if(!this.pinned) {
+            this.$element.removeClass(options.topClassName);
+            this.$element.css({ 'top': '' });
+        }
+
+
+        if((scrollHeight - screenHeight) + 10 <= scrollTop) {
+            if(!this.pinned) {
+                this.$element.addClass(options.bottomClassName);
+
+                this.pinned = true;
+            }
+        } else {
+            if(this.pinned) {
+                this.$element.removeClass(options.bottomClassName);
+
+                this.pinned = false;
+            }
+        }
+    }
+
+    var Miamed = exports.Miamed || {};
+
+    Miamed.Sticky = Sticky;
+
+    exports.Miamed = Miamed;
+})(window.jQuery, window);
